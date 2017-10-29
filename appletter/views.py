72b851bccac6e7ba5aeee9b4b9e82cf05f5515e9
Владимир Бибин <devbibinva@gmail.com>
2017-10-29 16:05:30 +0300

    Database added

--- appletter/views.py
+++ appletter/views.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+import requests
+import urllib
+import json 
+import datetime
+import time
+import numpy as np
+import os
+
+TOKEN = "5357446710.0be8de7.e8bbb791ba7f4ffdbc44ef1977cd49a3"
+
 # Create your views here.
--- letter_analys/settings.py
+++ letter_analys/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'letter_analys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'letter_analys',
+        'USER': 'vladimir',
+        'PASSWORD': '000',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
