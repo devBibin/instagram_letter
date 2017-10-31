458eb0b7506b099ee5aab79d4917b8c8ca68fb7c
Владимир Бибин <devbibinva@gmail.com>
2017-11-01 01:05:10 +0300

    Heroku [9]

--- appletter/urls.py
+++ appletter/urls.py
@@ -6,7 +6,3 @@ from . import views
 urlpatterns = [
     url(r'^(?P<user_name>.*)/', views.create_letter, name='create_letter'),
 ]
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
--- letter_analys/settings.py
+++ letter_analys/settings.py
@@ -49,10 +49,13 @@ else:
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 ALLOWED_HOSTS = ["*"]
 
+
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, '../appletter/static'),
