59a1cea5a9380f752754f56cba3950ff0199c4ab
Владимир Бибин <devbibinva@gmail.com>
2017-11-01 00:55:14 +0300

    Heroku [8]

--- letter_analys/settings.py
+++ letter_analys/settings.py
@@ -54,6 +54,7 @@ ALLOWED_HOSTS = ["*"]
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, '../appletter/static'),
     )
 
